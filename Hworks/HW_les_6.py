c5407dff01e9253d6007d45704fd64548ebaa132
Andrew <andrej.salnikov.03@bk.ru>
2023-09-22 20:14:13 +0300

    feat: Completed HW num.6

---
+++ Hworks/HW_les_6.py
@@ -0,0 +1,30 @@
+'''
+Задача 30:  Заполните массив элементами арифметической прогрессии. Её первый элемент, разность и количество элементов 
+нужно ввести с клавиатуры. Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+Каждое число вводится с новой строки.
+
+Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону 
+(т.е. не меньше заданного минимума и не больше заданного максимума)
+'''
+
+# def arif_prog(array: list[int], a_1: int, d: int, n: int) -> list[int]:
+#     array = [a_1 + i * d for i in range(n)]
+#     return array
+
+# first_element = int(input('Введите первый член арифм. прогрессии: '))
+# difference = int(input('Введите разность арифм. прогрессии: '))
+# elements = int(input('Введите сколько членов арифм. прогрессии нужно вывести: '))
+# list = arif_prog(array=list, a_1=first_element, d=difference, n=elements)
+# print(*list)
+
+from random import randint as rnd
+
+minimum = int(input('Введите минимальное число диапозона поиска: '))
+maximum = int(input('Введите максимальное число диапозона поиска: '))
+list = [rnd(1, 50) for _ in range(1, 10)]
+list_new = []
+for i in range(len(list)):
+    if minimum < list[i] < maximum:
+        list_new.append(i)
+print(f'{len(list)} elements in {list}')
+print(list_new)
