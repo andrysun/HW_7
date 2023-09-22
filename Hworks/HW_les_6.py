41d028828a9242b834b313eade7674f544322209
Andrew <andrej.salnikov.03@bk.ru>
2023-09-22 20:23:57 +0300

    modified hw 6

--- Hworks/HW_les_6.py
+++ Hworks/HW_les_6.py
@@ -7,17 +7,18 @@
 (т.е. не меньше заданного минимума и не больше заданного максимума)
 '''
 
-# def arif_prog(array: list[int], a_1: int, d: int, n: int) -> list[int]:
-#     array = [a_1 + i * d for i in range(n)]
-#     return array
+def arif_prog(array: list[int], a_1: int, d: int, n: int) -> list[int]:
+    array = [a_1 + i * d for i in range(n)]
+    return array
 
-# first_element = int(input('Введите первый член арифм. прогрессии: '))
-# difference = int(input('Введите разность арифм. прогрессии: '))
-# elements = int(input('Введите сколько членов арифм. прогрессии нужно вывести: '))
-# list = arif_prog(array=list, a_1=first_element, d=difference, n=elements)
-# print(*list)
+first_element = int(input('Введите первый член арифм. прогрессии: '))
+difference = int(input('Введите разность арифм. прогрессии: '))
+elements = int(input('Введите сколько членов арифм. прогрессии нужно вывести: '))
+list = arif_prog(array=list, a_1=first_element, d=difference, n=elements)
+print(*list)
 
 from random import randint as rnd
+
 
 minimum = int(input('Введите минимальное число диапозона поиска: '))
 maximum = int(input('Введите максимальное число диапозона поиска: '))
